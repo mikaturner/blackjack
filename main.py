bf914b9139d19333c52ff40af38498f40ff79edd
Mika Turner <69220717+mikaturner@users.noreply.github.com>
2023-01-25 03:05:41 +0000

    Implemented most of black jack rules.  Still need to check for errors and potentially simplify with some function abstraction.

--- main.py
+++ main.py
@@ -96,27 +96,30 @@ dealer_total = hand_total(dealer_hand)
 
 #How to pull values out of tuple list for each hand  
 
-first_plr_card = (player_hand[0])[0]
-second_plr_card = (player_hand[1])[0]
-
 first_dlr_card = (dealer_hand[0])[0]
-second_dlr_card = (dealer_hand[1])[0]
 
-def player_string(hand):
+def hand_string(hand, total, subject):
   '''Creates a string to describe player hand depending on current cards in hand'''
-  part_1 = "Your hand is "
+  part_1 = f"{subject} hand: "
   card_string = ""
   just_cards = []
   for card in hand:
       just_cards.append(card[0])
   card_string = " and ".join(just_cards)        
-  part_2 = f" for a total of {player_total}."
+  part_2 = f" for a total of {total}."
   return part_1 + card_string + part_2
 
-print(player_string(player_hand))  
-#print(f"Your hand is {first_plr_card} and {second_plr_card} for a total of {player_total}")
+print(hand_string(player_hand, player_total, "Your"))  
 
 print(f"Dealer's first card is {first_dlr_card}")
+
+#def addtnl_card (hand, total, subject):
+#  new_card = random_card(1)
+#  hand += new_card
+#  total= hand_total(hand)
+#  if total >= 21:
+#    print("Bust!")
+#  print(hand_string(hand, total, subject))
 
 #Black Jack Game Logic
 
@@ -124,28 +127,57 @@ print(f"Dealer's first card is {first_dlr_card}")
   #If yes then check if dealer additional one card hand equals 21
     #Tie 
 if player_total == 21 & dealer_total == 21:
+    print(f"{hand_string(dealer_hand, dealer_total, 'Dealer')}")
     print("It's a Tie!")
 
 #elif Player hand == 21 and dealer hand <21 or >21 Player Wins
 elif player_total == 21:
+    print(f"{hand_string(dealer_hand, dealer_total, 'Dealer')}")
     print("You Win!")
 
 #elif player hand is < 21 ask player if they want another card (add this card to total)  
-elif player_total < 21:
+hit_stand = True
+while hit_stand:
+  if player_total < 21:
     another_card = (input("Do you want another card? 'y'or'n': ")).lower()
     if another_card == 'y':
         new_card = random_card(1)
         player_hand += new_card
-        hand_total(player_hand)  
+        player_total= hand_total(player_hand)
+        print(hand_string(player_hand, player_total, "Your"))
+       # addtnl_card(player_hand, player_total, "Your")
+    
+        if player_total == 21 and dealer_total >= 17:
+          print(f"{hand_string(dealer_hand, dealer_total, 'Dealer')}")
+          print("You Win!")
+          hit_stand = False
+    else: 
+        hit_stand = False
+ #Else if player hand is < 21 and they hold move on to dealer Hand
+#else:
+while dealer_total < 17 and player_total <= 21:
+  new_card = random_card(1)
+  dealer_hand += new_card
+  dealer_total = hand_total(dealer_hand)
+  print(hand_string(dealer_hand, dealer_total, "Dealer"))
+  #addtnl_card(dealer_hand, dealer_total, "Dealer")
 
-    #If total is >21 player Busts and Dealer Wins
-    #If player hand is <21 ask if they want another card and repeat  step two logic if they do want another card
-    #Elif if player hand is < 21 and they hold move on to dealer Hand
-#3 Dealer hand, add an additional card 
-  #if dealer hand > 21 
-      # Dealer Busts
-  #if dealer hand < 17
-      # Add another card to dealer hand and check above logic (aka if >21 dealer busts)
-  #else draw another card and check if > 21 (bust)
-  # or < 17 draw
+#player bust and dealer does not bust
+if player_total > 21 and dealer_total <= 21:
+    print (f"Bust! Dealer Wins with {hand_string(dealer_hand, dealer_total, 'Dealer')}")
+elif player_total >21 and dealer_total > 21:
+    print(f"Player and Dealer Both Bust, Dealer with {hand_string(dealer_hand, dealer_total, 'Dealer')} , Dealer Wins by Default")
+#if player and dealer tie
+elif dealer_total == player_total:
+  print(f"You tied with your score of {player_total} and {hand_string(dealer_hand, dealer_total, 'Dealer')}")
+ #if dealer hand > 21 # Dealer Busts  
+elif dealer_total > 21:
+  print(f"Dealer Busts with {hand_string(dealer_hand, dealer_total, 'Dealer')}. You Win!")
+elif dealer_total > player_total:
+  print(f"Dealer wins with {hand_string(dealer_hand, dealer_total, 'Dealer')}. You lose with your total of {player_total}.")
+elif player_total > dealer_total and player_total < 21:
+  print(f"You win with {player_total}. Dealer loses with {hand_string(dealer_hand, dealer_total, 'Dealer')}")
+
+      
+  
   #if !<17 and !>21 compare dealer hand value to player hand value, whomever has highest hand wins, if scores are == then tie
